5aff8f1dd9ef88cb422edbd62464bfc603545bb7
BuildTools <unconfigured@null.spigotmc.org>
2020-12-22 21:15:36 +0900

    first

---
+++ src/main/java/net/serveron/mcstar/customlistener/Listener/Milk.java
@@ -0,0 +1,10 @@
+package net.serveron.mcstar.customlistener.Listener;
+
+import net.serveron.mcstar.customlistener.CustomListener;
+
+public class Milk {
+    CustomListener plugin;
+    public Milk(CustomListener plugin){
+        this.plugin = plugin;
+    }
+}
